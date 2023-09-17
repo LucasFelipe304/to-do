b943d9343ccc5d593c0651d4e2c2031336b066e1
Lucas Felipe <devlucasfelipe@gmail.com>
2023-09-17 08:19:20 -0300

    Adding the option to delete an option

--- to-do.py
+++ to-do.py
@@ -1,22 +1,39 @@
 general_list = []
-while True:
-  def allOptions():
-    print('1 | Add item ')
-    print('2 | Change list')
-    print('3 | Delete item')
+def space():
+  print()
+space()
 
-  allOptions()
+def allOptions():
+  stringChangeYourOption = ('Change your option:')
+  print(stringChangeYourOption)
+  space()
+  print('1 | Add item ')
+  print('2 | Delete item')
+  print('3 | Change list')
+  print('4 | View list')
 
 
-  print('Your list is empty.')
-  userAddItem = str(input('Enter what you want to add: '))
-  general_list.append(userAddItem)
+userAddItem = str(input('Firstly, enter what you want to add: '))
+general_list.append(userAddItem)
+print('--- > Added item!')
+space()
+
+allOptions()
+
+variableChangeYourOption = str(input('Change your option: '))
+if variableChangeYourOption == '2':
+  for item in enumerate(general_list):
+    print(item)
+  getListIndex = int(input('Enter the index of the item you want to change: ')) 
+  print(f'--- > Item [{general_list[getListIndex]}] removed.!')
+  general_list.pop(getListIndex)
   
-  for item in general_list:
-    print('-', item)
+
+
 
 
 
   
 
 
+ 
