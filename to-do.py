2113a91965c4f8935f9cbfe32e2175d59bd6249b
Lucas Felipe <devlucasfelipe@gmail.com>
2023-09-16 21:56:39 -0300

    First bug fix attempt

--- to-do.py
+++ to-do.py
@@ -1,18 +1,19 @@
+
 def allOptions():
   print('1 | Add item ')
   print('2 | Change list')
   print('3 | Delete item')
 
 allOptions()
+general_list = []
 
+user = str(input('What do you want to-do: '))
+print('--->', user)
 
-def userChoice():
-  user = str(input('What do you want to-do: '))
-  print('--->', user)
-  return user
-
-userChoice()
-
-
+if user == '1':
+  addItem = str(input('Add item: '))
+  general_list.append(addItem)
+  print(general_list)
+  
 
 
