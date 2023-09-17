ac9074d5fd7ce9fd6c7e8863dc228daa981c4d23
Lucas Felipe <devlucasfelipe@gmail.com>
2023-09-17 10:16:33 -0300

    Bug fix

--- to-do.py
+++ to-do.py
@@ -3,6 +3,7 @@ def space():
   print()
 space()
 
+
 def allOptions():
   stringChangeYourOption = ('Change your option:')
   print(stringChangeYourOption)
@@ -11,18 +12,22 @@ def allOptions():
   print('2 | Delete item')
   print('3 | View list')
 
-
-userAddItem = str(input('Firstly, enter what you want to add: '))
-general_list.append(userAddItem)
-print('--- > Added item!')
+while True:
+  userAddItem = str(input('Firstly, enter what you want to add: '))
+  if len(userAddItem) > 0:
+    general_list.append(userAddItem)
+    print('--- > Added item!')
+    break
 
 space()
+
+
 allOptions()
 
 
 variableChangeYourOption = str(input('Change your option: '))
 
-if variableChangeYourOption == '2':
+if variableChangeYourOption == '2' and len(general_list) > 0:
   def userDeleteItem():
     for item in enumerate(general_list):
       print(item)
@@ -30,13 +35,19 @@ if variableChangeYourOption == '2':
     print(f'--- > Item [{general_list[getListIndex]}] removed.!')
     general_list.pop(getListIndex)
     return general_list
-  userDeleteItem() 
+  userDeleteItem()
+elif variableChangeYourOption == '2' and len(general_list) <= 0:
+  print("""You can't remove items because your list it's empty.""")
 
 if variableChangeYourOption == '3':
   def userViewList():
+    i = 0
     for item in general_list:
-      print('-', item)
+      print(f'{i} -', item)
+      i += 1
+    return general_list
   userViewList()
+
 
  
 
