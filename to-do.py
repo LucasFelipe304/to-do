7c2e99db86710aef28db3dc5247b6fcf1b33c4f6
Lucas Felipe <devlucasfelipe@gmail.com>
2023-09-17 10:57:18 -0300

    Improving the code

--- to-do.py
+++ to-do.py
@@ -3,43 +3,64 @@ def space():
   print()
 space()
 
+def sep():
+  print(end='  ')
 
+def lines():
+  print('_' * 20)
 def allOptions():
-  stringChangeYourOption = ('Change your option:')
-  print(stringChangeYourOption)
+  lines()
   space()
   print('1 | Add item ')
   print('2 | Delete item')
   print('3 | View list')
+  lines()
 
 while True:
   def AddItem():
     userAddItem = str(input('Enter what you want to add: '))
     general_list.append(userAddItem)
-    print(f'--- > Added item! [{userAddItem}]')
+    print(f'-- > Added item! [{userAddItem}]')
     return general_list
   break
 
 allOptions()
+
+
 while True:
-
-  variableChangeYourOption = str(input('Change your option: '))
-  if variableChangeYourOption == '1':
+  choiceUserOption = str(input('Change your option: '))
+  if choiceUserOption == '1':
+    space()
+    space()
+    print('| Add item')
+    sep()
     AddItem()
+    space()
+    space()
 
-  if variableChangeYourOption == '2' and len(general_list) > 0:
+  if choiceUserOption == '2' and len(general_list) > 0:
     def userDeleteItem():
+      space()
+      space()
+      print('| Delete item')
       for item in enumerate(general_list):
+        sep()
         print(item)
+      space()
+      sep()
       getListIndex = int(input('Enter the index of the item you want to change: ')) 
-      print(f'--- > Item [{general_list[getListIndex]}] removed.!')
+      print(f'-- > Item [{general_list[getListIndex]}] removed.!')
       general_list.pop(getListIndex)
       return general_list
     userDeleteItem()
-  elif variableChangeYourOption == '2' and len(general_list) <= 0:
-    print("""You can't remove items because your list it's empty.""")
+    space()
+    space()
 
-  if variableChangeYourOption == '3':
+
+  elif choiceUserOption == '2' and len(general_list) <= 0:
+    print("You can't remove items because your list it's empty.")
+
+  if choiceUserOption == '3' and len(general_list) > 0:
     def userViewList():
       i = 0
       for item in general_list:
@@ -47,7 +68,10 @@ while True:
         i += 1
       return general_list
     userViewList()
-
+    
+  elif choiceUserOption == '3' and len(general_list) <= 0:
+    print("You can't to see items because your list it's empty.")
+    space()
 
  
 
