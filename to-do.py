01139b39c47cdad158e119c6da5de2d912f94d0a
Lucas Felipe <devlucasfelipe@gmail.com>
2023-09-17 09:17:58 -0300

    Adding the option to view list

--- to-do.py
+++ to-do.py
@@ -9,25 +9,36 @@ def allOptions():
   space()
   print('1 | Add item ')
   print('2 | Delete item')
-  print('3 | Change list')
-  print('4 | View list')
+  print('3 | View list')
 
 
 userAddItem = str(input('Firstly, enter what you want to add: '))
 general_list.append(userAddItem)
 print('--- > Added item!')
-space()
 
+space()
 allOptions()
 
+
 variableChangeYourOption = str(input('Change your option: '))
+
 if variableChangeYourOption == '2':
-  for item in enumerate(general_list):
-    print(item)
-  getListIndex = int(input('Enter the index of the item you want to change: ')) 
-  print(f'--- > Item [{general_list[getListIndex]}] removed.!')
-  general_list.pop(getListIndex)
-  
+  def userDeleteItem():
+    for item in enumerate(general_list):
+      print(item)
+    getListIndex = int(input('Enter the index of the item you want to change: ')) 
+    print(f'--- > Item [{general_list[getListIndex]}] removed.!')
+    general_list.pop(getListIndex)
+    return general_list
+  userDeleteItem() 
+
+if variableChangeYourOption == '3':
+  def userViewList():
+    for item in general_list:
+      print('-', item)
+  userViewList()
+
+ 
 
 
 
